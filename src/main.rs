8f7ee6f1286a407f088aefafc1322f797c521537
Abhinav Tushar <abhinav@lepisma.xyz>
2025-02-05 22:57:31 +0530

    Use better namespace

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use games::Validate;
+use games::{azul::{self, play_greedy}, Validate};
 
 mod games;
 
@@ -6,16 +6,23 @@ fn main() {
     env_logger::init();
 
     let n_players: usize = 3;
-    let mut state = games::azul::State::new(n_players);
+    let players = [
+        azul::play_greedy,
+        azul::play_greedy,
+        azul::play_with_lookahead,
+    ];
+
+    let mut state = azul::State::new(n_players);
 
     if let Err(err) = state.validate() {
         println!("{}", err);
         return;
     }
 
+
     loop {
         log::debug!("Round: {}", state.rounds);
-        let mut current_player = match games::azul::first_player(&state) {
+        let mut current_player = match azul::first_player(&state) {
             Some(one) => {
                 state.players[one].starting_marker = false;
                 one
@@ -25,25 +32,25 @@ fn main() {
 
         log::debug!("Starting player: {}", current_player);
 
-        games::azul::refill_tiles(&mut state);
+        azul::refill_tiles(&mut state);
         loop {
             // If tiles are over, round stops
             if state.has_no_tiles() {
                 state.rounds += 1;
                 break;
             }
-            log::info!("Picking from {} actions for P{}", games::azul::list_valid_actions(&state, current_player).len(), current_player);
-            games::azul::play_greedy(&mut state, current_player);
+            log::info!("Picking from {} actions for P{}", azul::list_valid_actions(&state, current_player).len(), current_player);
+            players[current_player](&mut state, current_player);
             current_player += 1;
             current_player %= n_players;
         }
 
         for i in 0..n_players {
-            games::azul::tile_wall_and_score(&mut state, i);
+            azul::tile_wall_and_score(&mut state, i);
             log::debug!("Score P{}: {}", i, state.players[i].score);
         }
 
-        if games::azul::is_game_over(&state) {
+        if azul::is_game_over(&state) {
             break;
         }
     }
@@ -52,5 +59,5 @@ fn main() {
         log::info!("Final score P{}: {}", i, state.players[i].score);
     }
 
-    log::info!("Winner is P{}", games::azul::winner(&state));
+    log::info!("Winner is P{}", azul::winner(&state));
 }
